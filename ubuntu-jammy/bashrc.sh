63fad56bfc8bcfd7701bd7e969712199ca3c4ab8
Carlo Mancini <carlo.mancini-terracciano@uniroma1.it>
2024-03-18 15:17:40 +0100

    improving gui

--- ubuntu-jammy/bashrc.sh
+++ ubuntu-jammy/bashrc.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+cat /etc/lsb-release
+
 export HISTCONTROL=ignoredups
 
 alias cd..='cd ..'
@@ -29,15 +32,18 @@ echo " "
 echo "To keep the size of the Docker images limited, the Geant4 datasets are not installed."
 echo "They are expect to be found in /opt/geant4/data"
 echo "I suggest you to map a folder in the host to use always the same dataset with the option:"
-echo "--volume=\"SOME_PATH/geant4-data:/opt/geant4/data:ro\" "
+echo "--volume=\'HOST_PATH/geant4-data:/opt/geant4/data:ro\' "
 echo "If some, or all, are missing it is possible to install the datasets from the docker with:"
 echo "geant4-config --install-datasets"
 echo "in this case, if you want to dowload the datasets in a host folder,"
 echo "you must mount the volume without the read-only flag:"
-echo "--volume=\"SOME_PATH/geant4-data:/opt/geant4/data\" "
+echo "--volume=\'HOST_PATH/geant4-data:/opt/geant4/data\' "
 echo "#############################################################################################"
 echo " "
 echo "## Geant4 examples ##########################################################################"
 echo "To save space, Geant4 examples have been removed, to download them:"
-echo "curl https://gitlab.cern.ch/geant4/geant4/-/archive/geant4-`geant4-config --version | awk -F '.' '{print $1"."$2}'`-release/geant4-master.tar.gz?path=examples --output examples.tar.gz && tar xf examples.tar.gz --strip-components 1"
+echo "curl https://gitlab.cern.ch/geant4/geant4/-/archive/geant4-`/opt/geant4/bin/geant4-config --version | awk -F '.' '{print $1"."$2}'`-release/geant4-master.tar.gz?path=examples --output examples.tar.gz && tar xf examples.tar.gz --strip-components 1"
 echo " "
+
+# Execute the command passed to the Docker container
+exec "$@"
